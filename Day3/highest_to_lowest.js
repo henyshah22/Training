d71ba40e1d92f991b5bba461f748cdaec82e5c96
henyshah22 <heny.shah@vc-erp.com>
2025-05-21 15:06:57 +0530

    changes

--- Day3/highest_to_lowest.js
+++ Day3/highest_to_lowest.js
@@ -1,11 +1,18 @@
 // Write a JavaScript function that accepts a String with space-separated numbers and returns the highest and lowest number (as a string).
 // Ex. User Input : "1 2 3 4 5"
 //       Output : "5 1"
-function highLow(str) {
-  let arr = str.split(" ");
-  let max = Math.max(...arr); // Strings auto-convert to numbers
-  let min = Math.min(...arr);
-  return `${max} ${min}`;
+function highAndLow(numbers) {
+  const nums = numbers.split(" ");
+  let highest = parseInt(nums[0]);
+  let lowest = parseInt(nums[0]);
+  for (let i = 1; i < nums.length; i++) {
+    const num = parseInt(nums[i]);
+    if (num > highest) {
+      highest = num;
+    }
+    if (num < lowest) {
+      lowest = num;
+    }
+  }
+  return `${highest} ${lowest}`;
 }
-
-console.log(highLow("1 2 3 4 5"));
--- Day3/mul_table.js
+++ Day3/mul_table.js
@@ -1,25 +1,11 @@
 // "JavaScript program to display multiplication tables using user input integer value.
-
 // Ex. User Input: 2
-
 //      2 * 1 = 2
-
 //      2 * 2 = 4
-
 //      ….
-
 //      2 * 10 = 20"
-
-//  a way to read user input from the terminal
-const readline = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-readline.question("Enter a number: ", (n) => {
-  n = parseInt(n);
-  for (let i = 1; i <= 10; i++) {
-    console.log(`${n} * ${i} = ${n * i}`);
-  }
-  readline.close();
-});
+const number = parseInt(prompt("Enter an integer: "));
+for (let i = 1; i <= 10; i++) {
+  const result = i * number;
+  console.log(`${number} * ${i} = ${result}`);
+}
