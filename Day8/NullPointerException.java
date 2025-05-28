708df1c1e516c1a7a05a226a8a0be42955a4c389
henyshah22 <heny.shah@vc-erp.com>
2025-05-28 12:44:49 +0530

    npe

--- Day8/NullPointerException.java
+++ Day8/NullPointerException.java
@@ -1,10 +1,10 @@
 // Write a code to show NullPointerException.
 // A NullPointerException in Java is a RuntimeException. In Java, a special null value can be assigned to an object reference. NullPointerException is thrown when a program attempts to use an object reference that has the null value.
 
-package Day8;
-public class NullPointerException {
-    public static void main(String[] args) {
-        String s = null;
-        System.out.println(s.length());
-    }
-}
+// package Day8;
+// public class NullPointerException {
+//     public static void main(String[] args) {
+//         String s = null;
+//         System.out.println(s.length());
+//     }
+// }
